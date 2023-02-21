b8f36a7e24267b464161d4da406f7b6818aec888
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-21 19:55:00 +0000

    Added trees layer and created first scene to animation on scroll

--- js/main.js
+++ js/main.js
@@ -2,25 +2,33 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('.mountain', {
-  x: -1000,
-  y: 1000,
-  duration: 5,
-  scrollTrigger: {
-    trigger: '.scene-two',
-    start: 'top 30%',
-    end: 'bottom',
-    markers: true
-  }
+const speed = 100;
+
+const scene1 = gsap.timeline();
+ScrollTrigger.create({
+  animation: scene1,
+  trigger: '.scroll-element',
+  start: 'top top',
+  end: '45% 100%',
+  scrub: 3
 });
 
-gsap.to('.mountain2', {
-  x: -500,
-  y: 1000,
-  duration: 5,
-  scrollTrigger: {
-    trigger: '.scene-two',
-    start: 'top 30%',
-    end: 'bottom'
-  }
-});
+// gsap.to('.mountain', {
+//   x: -1000,
+//   y: 1000,
+//   duration: 5,
+//   scrollTrigger: {
+//     trigger: '.scene-two',
+//     start: 'top 30%',
+//     end: 'bottom',
+//     markers: true
+//   }
+// });
+
+scene1.to('.section-trees', {
+  y: 3 * speed,
+  x: 0,
+  scale: 1,
+  ease: 'power1.in'
+},
+0);
